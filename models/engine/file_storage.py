4e8cff637fc7739002d43c026ebde9ad52baa12c
Kodisang <119697169+Kodisang@users.noreply.github.com>
2023-06-24 23:56:05 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self,cls=None ):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None: 
+            return FileStorage.__objects
+        else: 
+            cls_dict = {}
+             for key, obj in FileStorage.__objects.items():
+                 if type(obj).__name__ == cls.__name__:
+                     cls_dict[key] = obj
+                     return cls_dict
+            
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +63,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+def delete(self, obj=None):
+        """Delete obj from __objects if it's inside"""
+        if obj is not None: 
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                
+                del FileStorage.__objects[key]
+                self.save()
